21cd0692929819392ffb38f517658f2bf9be9e36
SRFeesha <SRFeesha@gmail.com>
2020-03-07 14:58:59 +0100

    styled header

--- header.php
+++ header.php
@@ -28,23 +28,10 @@
 		<div class="site-branding">
 			<?php
 			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$alma_wp_description = get_bloginfo( 'description', 'display' );
-			if ( $alma_wp_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $alma_wp_description; /* WPCS: xss ok. */ ?></p>
-			<?php endif; ?>
+			?>
 		</div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation">
+		<nav id="site-navigation" class="main-navigation col-10">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'alma-wp' ); ?></button>
 			<?php
 			wp_nav_menu( array(

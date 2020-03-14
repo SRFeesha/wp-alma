7f655f577a56426c73626c42fdaec80053e3ec9c
SRFeesha <SRFeesha@gmail.com>
2020-03-14 16:37:31 +0100

    styled portfolio card

--- template-parts/template-portfolio.php
+++ template-parts/template-portfolio.php
@@ -1,25 +1,47 @@
 <?php
     /*Template Name: Portfolio*/
     get_header();
-    query_posts(array(
-       'post_type' => 'case_study'
-    ));
-    while (have_posts()) : the_post(); ?>
-        <a href="<?php the_permalink() ?>">
-            <div class="card">
-                <div class="card-image">
-                    <?php the_post_thumbnail('medium', ['class' => 'card-featured-img']) ?>
-                </div>
-                <div class="card-content">
-                    <p class="card-over-title">
-                        <?php echo get_post_custom_values('settore')[0] ?> 
-                    </p>    
-                    <h3 class="card-title">
-                        <?php the_title() ?>
-                    </h3>
-                </div>
-            </div>
-        </a>
-    <?php endwhile;
-    get_footer();
 ?>
+
+<div id="container">
+    <div id="content" class="pageContent">
+        <h1 class="entry-title margin-top-7"><?php the_title(); ?></h1> <!-- Page Title -->
+        <?php
+            // TO SHOW THE PAGE CONTENTS
+            while ( have_posts() ) : the_post(); 
+        ?> <!--Because the_content() works only inside a WP Loop -->
+        <div class="entry-content-page margin-top-5">
+            <?php the_content(); ?> <!-- Page Content -->
+        </div><!-- .entry-content-page -->
+        <?php
+            endwhile; //resetting the page loop
+            wp_reset_query(); //resetting the page query
+        ?>
+
+        <div class="card-grid margin-top-9">
+            <?php
+                query_posts(array(
+                'post_type' => 'case_study'
+                ));
+                while (have_posts()) : the_post(); ?>
+                    <div class="card">
+                        <a class="card-link" href="<?php the_permalink() ?>">
+                            <div class="card-image">
+                                <?php the_post_thumbnail('full', ['class' => 'card-featured-img']) ?>
+                            </div>
+                            <div class="card-content">
+                                <p class="card-over-title caption">
+                                    <?php echo get_post_custom_values('settore')[0] ?> 
+                                </p>    
+                                <h2 class="card-title">
+                                    <?php the_title() ?>
+                                </h2>
+                            </div>
+                        </a>
+                    </div>
+                <?php endwhile;
+                get_footer();
+            ?>
+        </div>
+    </div>
+</div>

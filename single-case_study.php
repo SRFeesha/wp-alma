e16dacab9151f4810bff114701b0169066f6ae84
SRFeesha <SRFeesha@gmail.com>
2020-03-16 17:03:57 +0100

    stylized case study page

--- single-case_study.php
+++ single-case_study.php
@@ -22,8 +22,16 @@ get_header();
 
 			// get_template_part( 'template-parts/content', get_post_type() );
 
-			the_post_navigation();
+			// the_post_navigation();
+			the_post_navigation( array(
+				'prev_text'                  => __( '← Caso studio precedente: <em> %title </em>' ),
+				'next_text'                  => __( 'Caso studio successivo: %title →' ),
+				// 'in_same_term'               => true,
+				// 'taxonomy'                   => __( 'post_tag' ),
+				'screen_reader_text' => __( 'Continue Reading' ),
+			) );
 
+			
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
 				comments_template();

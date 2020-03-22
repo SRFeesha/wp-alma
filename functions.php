72e33f054a277a8cced17ddbd715c411d2c9de6d
SRFeesha <SRFeesha@gmail.com>
2020-03-22 12:12:13 +0100

    styled service page

--- functions.php
+++ functions.php
@@ -373,111 +373,139 @@ add_theme_support( 'editor-font-sizes', array(
 ) );
 
 
-    /*
-     * Function for post duplication. Dups appear as drafts. User is redirected to the edit screen
-     */
-    function rd_duplicate_post_as_draft(){
-		global $wpdb;
-		if (! ( isset( $_GET['post']) || isset( $_POST['post'])  || ( isset($_REQUEST['action']) && 'rd_duplicate_post_as_draft' == $_REQUEST['action'] ) ) ) {
-		  wp_die('No post to duplicate has been supplied!');
+/*
+* Function for post duplication. Dups appear as drafts. User is redirected to the edit screen
+*/
+function rd_duplicate_post_as_draft(){
+	global $wpdb;
+	if (! ( isset( $_GET['post']) || isset( $_POST['post'])  || ( isset($_REQUEST['action']) && 'rd_duplicate_post_as_draft' == $_REQUEST['action'] ) ) ) {
+		wp_die('No post to duplicate has been supplied!');
+	}
+
+	/*
+	* Nonce verification
+	*/
+	if ( !isset( $_GET['duplicate_nonce'] ) || !wp_verify_nonce( $_GET['duplicate_nonce'], basename( __FILE__ ) ) )
+		return;
+
+	/*
+	* get the original post id
+	*/
+	$post_id = (isset($_GET['post']) ? absint( $_GET['post'] ) : absint( $_POST['post'] ) );
+	/*
+	* and all the original post data then
+	*/
+	$post = get_post( $post_id );
+
+	/*
+	* if you don't want current user to be the new post author,
+	* then change next couple of lines to this: $new_post_author = $post->post_author;
+	*/
+	$current_user = wp_get_current_user();
+	$new_post_author = $current_user->ID;
+
+	/*
+	* if post data exists, create the post duplicate
+	*/
+	if (isset( $post ) && $post != null) {
+
+		/*
+		* new post data array
+		*/
+		$args = array(
+		'comment_status' => $post->comment_status,
+		'ping_status'    => $post->ping_status,
+		'post_author'    => $new_post_author,
+		'post_content'   => $post->post_content,
+		'post_excerpt'   => $post->post_excerpt,
+		'post_name'      => $post->post_name,
+		'post_parent'    => $post->post_parent,
+		'post_password'  => $post->post_password,
+		'post_status'    => 'draft',
+		'post_title'     => $post->post_title,
+		'post_type'      => $post->post_type,
+		'to_ping'        => $post->to_ping,
+		'menu_order'     => $post->menu_order
+		);
+
+		/*
+		* insert the post by wp_insert_post() function
+		*/
+		$new_post_id = wp_insert_post( $args );
+
+		/*
+		* get all current post terms ad set them to the new post draft
+		*/
+		$taxonomies = get_object_taxonomies($post->post_type); // returns array of taxonomy names for post type, ex array("category", "post_tag");
+		foreach ($taxonomies as $taxonomy) {
+		$post_terms = wp_get_object_terms($post_id, $taxonomy, array('fields' => 'slugs'));
+		wp_set_object_terms($new_post_id, $post_terms, $taxonomy, false);
 		}
-	   
+
 		/*
-		 * Nonce verification
-		 */
-		if ( !isset( $_GET['duplicate_nonce'] ) || !wp_verify_nonce( $_GET['duplicate_nonce'], basename( __FILE__ ) ) )
-		  return;
-	   
-		/*
-		 * get the original post id
-		 */
-		$post_id = (isset($_GET['post']) ? absint( $_GET['post'] ) : absint( $_POST['post'] ) );
-		/*
-		 * and all the original post data then
-		 */
-		$post = get_post( $post_id );
-	   
-		/*
-		 * if you don't want current user to be the new post author,
-		 * then change next couple of lines to this: $new_post_author = $post->post_author;
-		 */
-		$current_user = wp_get_current_user();
-		$new_post_author = $current_user->ID;
-	   
-		/*
-		 * if post data exists, create the post duplicate
-		 */
-		if (isset( $post ) && $post != null) {
-	   
-		  /*
-		   * new post data array
-		   */
-		  $args = array(
-			'comment_status' => $post->comment_status,
-			'ping_status'    => $post->ping_status,
-			'post_author'    => $new_post_author,
-			'post_content'   => $post->post_content,
-			'post_excerpt'   => $post->post_excerpt,
-			'post_name'      => $post->post_name,
-			'post_parent'    => $post->post_parent,
-			'post_password'  => $post->post_password,
-			'post_status'    => 'draft',
-			'post_title'     => $post->post_title,
-			'post_type'      => $post->post_type,
-			'to_ping'        => $post->to_ping,
-			'menu_order'     => $post->menu_order
-		  );
-	   
-		  /*
-		   * insert the post by wp_insert_post() function
-		   */
-		  $new_post_id = wp_insert_post( $args );
-	   
-		  /*
-		   * get all current post terms ad set them to the new post draft
-		   */
-		  $taxonomies = get_object_taxonomies($post->post_type); // returns array of taxonomy names for post type, ex array("category", "post_tag");
-		  foreach ($taxonomies as $taxonomy) {
-			$post_terms = wp_get_object_terms($post_id, $taxonomy, array('fields' => 'slugs'));
-			wp_set_object_terms($new_post_id, $post_terms, $taxonomy, false);
-		  }
-	   
-		  /*
-		   * duplicate all post meta just in two SQL queries
-		   */
-		  $post_meta_infos = $wpdb->get_results("SELECT meta_key, meta_value FROM $wpdb->postmeta WHERE post_id=$post_id");
-		  if (count($post_meta_infos)!=0) {
-			$sql_query = "INSERT INTO $wpdb->postmeta (post_id, meta_key, meta_value) ";
-			foreach ($post_meta_infos as $meta_info) {
-			  $meta_key = $meta_info->meta_key;
-			  if( $meta_key == '_wp_old_slug' ) continue;
-			  $meta_value = addslashes($meta_info->meta_value);
-			  $sql_query_sel[]= "SELECT $new_post_id, '$meta_key', '$meta_value'";
-			}
-			$sql_query.= implode(" UNION ALL ", $sql_query_sel);
-			$wpdb->query($sql_query);
-		  }
-	   
-	   
-		  /*
-		   * finally, redirect to the edit post screen for the new draft
-		   */
-		  wp_redirect( admin_url( 'post.php?action=edit&post=' . $new_post_id ) );
-		  exit;
-		} else {
-		  wp_die('Post creation failed, could not find original post: ' . $post_id);
+		* duplicate all post meta just in two SQL queries
+		*/
+		$post_meta_infos = $wpdb->get_results("SELECT meta_key, meta_value FROM $wpdb->postmeta WHERE post_id=$post_id");
+		if (count($post_meta_infos)!=0) {
+		$sql_query = "INSERT INTO $wpdb->postmeta (post_id, meta_key, meta_value) ";
+		foreach ($post_meta_infos as $meta_info) {
+			$meta_key = $meta_info->meta_key;
+			if( $meta_key == '_wp_old_slug' ) continue;
+			$meta_value = addslashes($meta_info->meta_value);
+			$sql_query_sel[]= "SELECT $new_post_id, '$meta_key', '$meta_value'";
 		}
-	  }
-	  add_action( 'admin_action_rd_duplicate_post_as_draft', 'rd_duplicate_post_as_draft' );
-	   
-	  /*
-	   * Add the duplicate link to action list for post_row_actions
-	   */
-	  function rd_duplicate_post_link( $actions, $post ) {
-		if (current_user_can('edit_posts')) {
-		  $actions['duplicate'] = '<a href="' . wp_nonce_url('admin.php?action=rd_duplicate_post_as_draft&post=' . $post->ID, basename(__FILE__), 'duplicate_nonce' ) . '" title="Duplicate this item" rel="permalink">Duplicate</a>';
+		$sql_query.= implode(" UNION ALL ", $sql_query_sel);
+		$wpdb->query($sql_query);
 		}
-		return $actions;
-	  }
+
+
+		/*
+		* finally, redirect to the edit post screen for the new draft
+		*/
+		wp_redirect( admin_url( 'post.php?action=edit&post=' . $new_post_id ) );
+		exit;
+	} else {
+		wp_die('Post creation failed, could not find original post: ' . $post_id);
+	}
+}
+add_action( 'admin_action_rd_duplicate_post_as_draft', 'rd_duplicate_post_as_draft' );
 	   
-	  add_filter( 'post_row_actions', 'rd_duplicate_post_link', 10, 2 );
+/*
+* Add the duplicate link to action list for post_row_actions
+*/
+function rd_duplicate_post_link( $actions, $post ) {
+	if (current_user_can('edit_posts')) {
+		$actions['duplicate'] = '<a href="' . wp_nonce_url('admin.php?action=rd_duplicate_post_as_draft&post=' . $post->ID, basename(__FILE__), 'duplicate_nonce' ) . '" title="Duplicate this item" rel="permalink">Duplicate</a>';
+	}
+	return $actions;
+}
+add_filter( 'post_row_actions', 'rd_duplicate_post_link', 10, 2 );
+
+
+	  
+// ----------
+// Custom Gutenberg environment	  
+// ----------
+function alma_gutenberg_css(){
+	add_theme_support( 'editor-styles' ); // if you don't add this line, your stylesheet won't be added
+	add_editor_style( 'style-editor.css' ); // tries to include style-editor.css directly from your theme folder
+}
+
+add_action( 'after_setup_theme', 'alma_gutenberg_css' );
+
+
+
+// Add the possibility to get a wide or full image size
+add_theme_support( 'align-wide' );
+
+
+
+// add support for SVG
+function add_file_types_to_uploads($file_types){
+	$new_filetypes = array();
+	$new_filetypes['svg'] = 'image/svg+xml';
+	$file_types = array_merge($file_types, $new_filetypes );
+	return $file_types;
+}
+add_filter('upload_mimes', 'add_file_types_to_uploads');
+ 
--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -11,7 +11,7 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		<?php the_title( '<h1 class="entry-title margin-top-7">', '</h1>' ); ?>
 	</header><!-- .entry-header -->
 
 	<?php alma_wp_post_thumbnail(); ?>

87829e3967b4466e03a196de707b829245639aa0
SRFeesha <SRFeesha@gmail.com>
2020-04-03 15:20:17 +0200

    adjusted arrangement of alma services on mobile

--- js/horizontal-slider.js
+++ js/horizontal-slider.js
@@ -7,13 +7,53 @@
  */
 ( function() {
 	var hs = document.querySelectorAll(".horizontal-slider")[0];
+	// var view = document.querySelectorAll(".horizontal-slider .container")[0];
+	var totalWidth = hs.scrollWidth;
+	var clientWidth = hs.clientWidth;
+	var move = 500;
+	var sliderLimit = clientWidth;
+	var actualPosition = clientWidth + hs.scrollLeft;
+
+	console.log("Total: " + totalWidth);
+	console.log("Client: " + clientWidth);
+	console.log("Percentuale visibile: " + clientWidth / totalWidth);
 
 	var rightArrow = document.createElement("div");
 	hs.appendChild(rightArrow); 
 	rightArrow.innerHTML = "→";
 	rightArrow.classList.add("arrow", "right-arrow");
-	// console.log(hs);
-	// hs.style.color = "purple";
+	// console.log(view);
+	// hs = view;
+
+	function moveRight(){    
+		console.log("Prima: " + actualPosition);
+		hs.scrollLeft += move;
+		console.log("Dopo: " + (clientWidth + hs.scrollLeft));
+		// if (currentPosition >= sliderLimit) view.stop(false,true).animate({left:"-="+move},{ duration: 400})
+	};
 	
+	if (clientWidth == totalWidth){
+		rightArrow.classList.add("greyed");
+	}
+	else {
+		rightArrow.classList.remove("greyed");
+	}
+
+	rightArrow.addEventListener('click', event => {
+		moveRight()
+	});
+
+	// rightArrow.click(moveRight());
 }
 )();
+
+
+
+
+
+// $("#leftArrow").click(function(){
+
+//     var currentPosition = parseInt(view.css("left"));
+//     if (currentPosition < 0) view.stop(false,true).animate({left:"+="+move},{ duration: 400})
+
+// });
